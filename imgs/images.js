f648853e59ac2edf98e68e6942c7f7829fb46f20
Rory Maguire <rorymaguire00@gmail.com>
2022-03-01 21:36:40 +0000

    get imgs

---
+++ imgs/images.js
@@ -0,0 +1,7 @@
+//silat beksi
+//src="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/sheep-5.jpg"
+//class="img-fluid img-thumbnail"
+
+//cristi cons
+//"https://imgproxy.ra.co/_/quality:100/w:1500/rt:fill/plain/https://static.ra.co/images/profiles/lg/cristicons.jpg?dateUpdated=1499961395000"
+//class="img-fluid img-thumbnail"
